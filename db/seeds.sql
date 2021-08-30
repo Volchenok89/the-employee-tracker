a1ab9962b8ff254afc56ae2ed57cf394a913da7e
Volchenok <warp.speed@hotmail.com>
2021-08-29 17:14:03 -0700

    names

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,21 +10,21 @@ VALUES
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-    ("Frontend Engineer", 150000, 1),
-    ("Backend Engineer", 150000, 1),
-    ("Product Manager", 130000, 2),
-    ("Support Specialist", 100000, 3),
-    ("Account Executive", 100000, 4),
+    ("Frontend Engineer", 200000, 1),
+    ("Backend Engineer", 200000, 1),
+    ("Product Manager", 160000, 2),
+    ("Support Specialist", 140000, 3),
+    ("Accounting Executive", 160000, 5),
     ("Sales Engineer", 120000, 4),
-    ("Accounts Receivable", 140000, 5);
+    ("Finance Treasurer", 120000, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
-    ("Patrick", "Lago", 1, 1),
-    ("Stephen", "Curry", 2, 2),
-    ("Klay", "Thompson", 3, null),
-    ("Draymond", "Green", 4, 3),
-    ("Steve", "Kerr", 5, 2),
-    ("James", "Wiseman", 6, null),
-    ("Andrew", "Wiggins", 7, 4),
-    ("Kevon", "Looney", 5, 2);
+    ("Mina", "Saberhagen", 1, 1),
+    ("Tonchi", "Castro", 2, 2),
+    ("Alexandra", "Karkali", 3, null),
+    ("Maggie", "Schmidt", 4, 3),
+    ("Arya", "Casiraghi", 5, 2),
+    ("Ruth", "Grimaldi", 6, null),
+    ("Gianni", "Gutti", 7, 4),
+    ("Salem", "Saberhagen", 5, 2);
