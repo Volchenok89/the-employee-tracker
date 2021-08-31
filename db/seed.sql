ade286a4599ef3ede22b9c07b180df4a2962bc18
Volchenok <warp.speed@hotmail.com>
2021-08-30 20:51:19 -0700

    data

--- db/seed.sql
+++ db/seed.sql
@@ -8,13 +8,13 @@ VALUES
     ("Acting"),
     ("Video"),
 
-INSERT INTO role(title, salary, department_id)
+INSERT INTO role (title, salary, department_id)
 VALUES
     ("Producer", 500000, 1),
-    ("Director", 400000, 1),
-    ("Video Manager", 200000, 2),
-    ("Staff", 60000, 3),
-    ("Accounting Executive", 200000, 4),
+    ("Director", 400000, 2),
+    ("Video Manager", 200000, 3),
+    ("Staff", 60000, 4),
+    ("Accounting Executive", 200000, 5),
 
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
