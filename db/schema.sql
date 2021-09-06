f0836fed7ce2672827d566ce6b22160448f4112a
Volchenok <warp.speed@hotmail.com>
2021-09-06 02:03:03 -0700

    roles

--- db/schema.sql
+++ db/schema.sql
@@ -19,8 +19,8 @@ CREATE TABLE employee (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  roles_id INTEGER,
+  role_id INTEGER,
   manager_id INTEGER NULL,
-  CONSTRAINT fk_roles FOREIGN KEY (roles_id) REFERENCES roles(id) ON DELETE SET NULL,
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
   CONSTRAINT fk_employee FOREIGN KEY (manager_id) REFERENCES employee(id) 
 );
